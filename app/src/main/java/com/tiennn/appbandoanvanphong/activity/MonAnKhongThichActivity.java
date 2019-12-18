a7d7ca7cbc5e7a959960c592cc65de1ac6ba1f6e
tienhn <tienhn270197@gmail.com>
2019-12-18 16:56:00 +0700

    first commit

---
+++ app/src/main/java/com/tiennn/appbandoanvanphong/activity/MonAnKhongThichActivity.java
@@ -0,0 +1,16 @@
+package com.tiennn.appbandoanvanphong.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tiennn.appbandoanvanphong.R;
+
+public class MonAnKhongThichActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mon_an_khong_thich);
+    }
+}
---
+++ app/src/main/java/com/tiennn/appbandoanvanphong/ultit/Server.java
@@ -0,0 +1,13 @@
+package com.tiennn.appbandoanvanphong.ultit;
+
+public class Server {
+    public static String localhost = "192.168.0.100";
+    public static String DuongdanLoaisp = "http://" + localhost + "/appbandoan/getloaisp.php";
+    public static String Duongdansanphammoinhat = "http://" + localhost + "/appbandoan/getsanphammoinhat.php";
+    public static String Duongdandoan = "http://" + localhost + "/appbandoan/getsanpham.php?page=";
+    public static String Duongdandonhang = "http://" + localhost + "/appbandoan/thongtinkhachhang.php";
+    public static String Duongdanchitietdonhang = "http://" + localhost + "/appbandoan/chitietdonhang.php";
+    public static String Duongdantimkiem = "http://" + localhost + "/appbandoan/gettimkiemsanpham.php";
+    public static String Duongdannguoidung = "http://" + localhost + "/appbandoan/getnguoidung.php";
+    public static String Duongdandangkynguoidung = "http://" + localhost + "/appbandoan/setnguoidung.php";
+}
